7695b2db0596127232efdcf859cab0c517be01c5
aneyeforsecrets <dirrgang@DESKTOP-1NVSC5A.localdomain>
2019-10-07 19:47:01 +0200

    Updated code

--- Firmware/MonsterMoto_Shield_Example_Sketch/MonsterMoto_Shield_Example_Sketch.ino
+++ Firmware/MonsterMoto_Shield_Example_Sketch/MonsterMoto_Shield_Example_Sketch.ino
@@ -1,6 +1,6 @@
 /*  MonsterMoto Shield Example Sketch
-  date: 5/24/11
-  code by: Jim Lindblom
+  date: 10/7/19
+  code by: Jim Lindblom & Dennis Irrgang
   hardware by: Nate Bernstein
   SparkFun Electronics
 
@@ -8,71 +8,102 @@
  functionality with the MonsterMoto Shield. The MonsterMote uses
  two VNH2SP30 high-current full-bridge motor drivers.
  
- Use the motorGo(uint8_t motor, uint8_t direct, uint8_t pwm) 
+ Use the motorGo(Motor motor, MotorMode mode, uint8_t speed) 
  function to control the motors. Available options are CW, CCW, 
- BRAKEVCC, or BRAKEGND. Use motorOff(int motor) to turn a specific motor off.
+ BRAKEVCC, or BRAKEGND. Use motorOff(Motor motor) to turn a specific motor off.
  
- The motor variable in each function should be either a 0 or a 1.
- pwm in the motorGo function should be a value between 0 and PWM_MAX.
+ The motor variable in each function should be a Motor enum, alternatively 0 or a 1.
+ pwm in the motorGo function should be a value between 0 and 255.
  
-This code is beerware; if you see me (or any other SparkFun employee) at the
+This code is beerware; if you see the authors (or any SparkFun employee) at the
 local, and you've found our code helpful, please buy us a round!
 
-Distributed as-is; no warranty is given.
- */
-#define BRAKEVCC 0
-#define CW 1
-#define CCW 2
-#define BRAKEGND 3
-#define CS_THRESHOLD 100
+Distributed as-is; no warranty is given. */
 
-#define PWM_MAX  255
-#define PWM_HALF 127
-
-/*  VNH2SP30 pin definitions
- xxx[0] controls motor '1' outputs
- xxx[1] controls motor '2' outputs */
-int inApin[2] = {7, 4}; // INA: Clockwise input
-int inBpin[2] = {8, 9}; // INB: Counter-clockwise input
-int pwmpin[2] = {5, 6}; // PWM input
-int cspin[2] = {2, 3};  // CS: Current sense ANALOG input
-int enpin[2] = {0, 1};  // EN: Status of switches output (Analog pin)
-
-int statpin = 13;
-
-void setup()
+enum MotorMode
 {
-  Serial.begin(9600);
+  BRAKEVCC,
+  CW,
+  CCW,
+  BRAKEGND,
+};
 
-  pinMode(statpin, OUTPUT);
+enum Motor
+{
+  MOTOR_A,
+  MOTOR_B,
+};
+
+const uint8_t PWM_MAX = 255;
+const uint8_t PWM_HALF = PWM_MAX / 2;
+
+const int kCurrentSensingThreshhold = 100;
+
+/* Voltage controlled input pins with hysteresis, CMOS compatible. These two pins
+control the state of the bridge in normal operation according to the truth table (brake
+to VCC , brake to GND, clockwise and counterclockwise).
+
+Table 1.    Truth table in normal operating conditions
++------+-----+------+-----+-------+-------+---------------------+------------------------+
+| INA  | INB | ENA  | ENB | OUTA  | OUTB  |         CS          |          MODE          |
++------+-----+------+-----+-------+-------+---------------------+------------------------+
+|    1 |   1 |    1 |   1 | H     | H     | High Imp.           | Brake to VCC           |
+|    1 |   0 |    1 |   1 | H     | L     | I_Sense = I_OUT / K | Clockwise (CW)         |
+|    0 |   1 |    1 |   1 | L     | H     | I_SENSE = I_OUT / K | Counterclockwise (CCW) |
+|    0 |   0 |    1 |   1 | L     | L     | High Imp.           | Brake to GND           |
++------+-----+------+-----+-------+-------+---------------------+------------------------+
+For more information, see the VNH2SP30-E motor driver datasheet */
+const int kInAPin[2] = {7, 4};
+const int kInBPin[2] = {8, 9};
+
+/* Voltage controlled input pins with hysteresis, CMOS compatible. Gates of low side
+FETs are modulated by the PWM signal during their ON phase allowing speed
+control of the motors. */
+const int kPwmPin[2] = {5, 6};
+
+/* When pulled low, disables the half-bridge of the VNH2SP30-E of the motor. In case of 
+fault detection (thermal shutdown of a high side FET or excessive ON state voltage drop 
+across a low side FET), this pin is pulled low by the device. 
+
+Table 2.    Truth table in fault conditions (detected on OUTA)
++------+-----+------+-----+-------+-------+------------+
+| INA  | INB | ENA  | ENB | OUTA  | OUTB  |     CS     |
++------+-----+------+-----+-------+-------+------------+
+| 1    | 1   |    1 |   1 | OPEN  | H     | High Imp.  |
+| 1    | 0   |    1 |   1 | OPEN  | L     | High Imp.  |
+| 0    | 1   |    1 |   1 | OPEN  | H     | I_OUTB / K |
+| 0    | 0   |    1 |   1 | OPEN  | L     | High Imp.  |
+| X    | X   |    0 |   0 | OPEN  | OPEN  | High Imp.  |
+| X    | 1   |    0 |   1 | OPEN  | H     | I_OUTB / K |
+| X    | 0   |    0 |   1 | OPEN  | L     | High Imp.  |
++------+-----+------+-----+-------+-------+------------+
+For more information, see the VNH2SP30-E motor driver datasheet */
+const int kEnPin[2] = {0, 1};
+
+/* Analog current sense input. This input senses a current proportional to the motor
+current. The information can be read back as an analog voltage. */
+const int kCspPin[2] = {2, 3};
+
+// On-Board LED used as status indication
+const int kStatPin = 13;
+
+void motorSetup()
+{
+  pinMode(kStatPin, OUTPUT);
 
   // Initialize digital pins as outputs
   for (int i = 0; i < 2; i++)
   {
-    pinMode(inApin[i], OUTPUT);
-    pinMode(inBpin[i], OUTPUT);
-    pinMode(pwmpin[i], OUTPUT);
+    pinMode(kInAPin[i], OUTPUT);
+    pinMode(kInBPin[i], OUTPUT);
+    pinMode(kPwmPin[i], OUTPUT);
   }
-  // Initialize with enabled brake
+  // Initialize with brake applied
   for (int i = 0; i < 2; i++)
   {
-    digitalWrite(inApin[i], LOW);
-    digitalWrite(inBpin[i], LOW);
+    digitalWrite(kInAPin[i], LOW);
+    digitalWrite(kInBPin[i], LOW);
   }
-}
-
-void loop()
-{
-  motorGo(0, CW, PWM_MAX);
-  motorGo(1, CCW, PWM_MAX);
-  delay(5000);
-
-  motorGo(0, CCW, PWM_MAX);
-  motorGo(1, CW, PWM_MAX);
-  delay(5000);
-
-  if ((analogRead(cspin[0]) < CS_THRESHOLD) && (analogRead(cspin[1]) < CS_THRESHOLD))
-    digitalWrite(statpin, HIGH);
 }
 
 void motorOff(int motor)
@@ -80,56 +111,96 @@ void motorOff(int motor)
   // Initialize brake to Vcc
   for (int i = 0; i < 2; i++)
   {
-    digitalWrite(inApin[i], LOW);
-    digitalWrite(inBpin[i], LOW);
+    digitalWrite(kInAPin[i], LOW);
+    digitalWrite(kInBPin[i], LOW);
   }
-  analogWrite(pwmpin[motor], 0);
+  analogWrite(kPwmPin[motor], 0);
 }
 
-/* motorGo() controls the motors. It allows control of their speed
- and direction. Once set the motor will continue with its previous
- settings indefinitely
+/* motorGo() will set a motor going in a specific direction
+ the motor will continue going in that direction, at that speed
+ until told to do otherwise.
  
- motor: Selects the motor to be controlled.
- Motor 1: 0
- Motor 2: 1
+ motor: this should be either MOTOR_A (0) or MOTOR_B (1), will select 
+ which of the two motors to be controlled. Invalid values are ignored.
  
- direct: Controls the direction in which the motor spins. Range is 0 - 3
- Settings have the following effect:
- 0: Brake to VCC
- 1: Spin Clockwise (CW)
- 2: Spin Counter-Clockwise (CCW)
- 3: Brake to GND
+ mode: Should be one of the following values
+  BRAKEVCC (0): Brake to VCC
+  CW (1): Turn Clockwise
+  CCW (2): Turn Counter-Clockwise
+  BRAKEGND (3): Brake to GND
  
- pwm: Range is between 0 (slowest)- 255 (fastest).
-
- Invalid values are ignored.
+ speed: should be a value between 0 and PWM_MAX (255), higher the number, the faster
  */
-void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)
+void motorGo(Motor motor, MotorMode mode, uint8_t speed)
 {
 
-  if (0 <= motor <= 1)
+  if (motor == MOTOR_A || motor == MOTOR_B)
   {
-    if (direct == 0 || 3)
-    { // Brake to Vcc
-      digitalWrite(inApin[motor], HIGH);
-      digitalWrite(inBpin[motor], HIGH);
+    switch (mode)
+    {
+    case BRAKEVCC: // Brake to VCC
+      digitalWrite(kInAPin[motor], HIGH);
+      digitalWrite(kInBPin[motor], HIGH);
+      break;
+    case CW: // Turn Clockwise
+      digitalWrite(kInAPin[motor], HIGH);
+      digitalWrite(kInBPin[motor], LOW);
+      break;
+    case CCW: // Turn Counter-Clockwise
+      digitalWrite(kInAPin[motor], LOW);
+      digitalWrite(kInBPin[motor], HIGH);
+      break;
+    case BRAKEGND: // Brake to GND
+      digitalWrite(kInAPin[motor], LOW);
+      digitalWrite(kInBPin[motor], LOW);
+      break;
+
+    default:
+      // Invalid mode does not change the PWM signal
+      return;
     }
-    else if (direct == 1)
-    { // Turn Clockwise
-      digitalWrite(inApin[motor], HIGH);
-      digitalWrite(inBpin[motor], LOW);
-    }
-    else if (direct == 2)
-    { // Turn Counter-Clockwise
-      digitalWrite(inApin[motor], LOW);
-      digitalWrite(inBpin[motor], HIGH);
+    analogWrite(kPwmPin[motor], speed);
+  }
+  return;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  motorSetup();
+}
+
+void loop()
+{
+  uint8_t speed;
+  float motorCurrent;
+
+  while (true)
+  {
+    motorGo(MOTOR_A, CW, PWM_MAX);
+    motorGo(MOTOR_B, CCW, PWM_MAX);
+    delay(5000);
+
+    motorOff(MOTOR_A);
+    motorOff(MOTOR_B);
+    delay(1000);
+
+    motorGo(MOTOR_A, CCW, PWM_MAX);
+    motorGo(MOTOR_B, CW, PWM_MAX);
+    delay(5000);
+
+    if ((analogRead(kCspPin[0]) < kCurrentSensingThreshhold) && (analogRead(kCspPin[1]) < kCurrentSensingThreshhold))
+    {
+      digitalWrite(kStatPin, HIGH);
     }
     else
     {
-      return;
+      digitalWrite(kStatPin, LOW);
+      break;
     }
-    analogWrite(pwmpin[motor], pwm);
   }
-  return;
+  motorOff(MOTOR_A);
+  motorOff(MOTOR_B);
 }
